80bc5b79455f5bd44f4db3e25019762955c73a1a
michael88273 <michaeljames18276@gmail.com>
2026-05-03 21:00:58 -0400

    Update Projects.tsx

--- app/sections/Projects.tsx
+++ app/sections/Projects.tsx
@@ -30,7 +30,6 @@ const Projects = () => {
                   name={project?.name}
                   description={project?.description}
                   image={project?.image}
-                  loomLink={project?.loomLink}
                   liveLink={project?.liveLink}
                   stacks={project?.stacks}
                   type={project?.type}
